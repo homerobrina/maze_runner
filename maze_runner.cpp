fbae521a74b1d0b0d147b59321885fb8a33cecee
homerobrina <96438142+homerobrina@users.noreply.github.com>
2023-08-29 22:36:01 +0000

    Entrega final.

--- maze_runner.cpp
+++ maze_runner.cpp
@@ -102,7 +102,7 @@ bool walk(pos_t pos) {
 
 	printf("posição: %d %d\n", pos.i, pos.j);
 	maze[pos.i][pos.j] = 'o';
-	std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	std::this_thread::sleep_for(std::chrono::milliseconds(110));
 	system("clear");
 	print_maze();
 	maze[pos.i][pos.j] = '.';
